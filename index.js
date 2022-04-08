2a4dd900dbe8123a818f85f070a581f5467ebb7d
IrasemaSerrano <irmaserranoji@ittepic.edu.mx>
2022-04-08 12:33:51 -0500

    cashboxes CRUD and routes

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ app.use( cors() );
 //ROUTES
 const businessunitsRoutes = require('./routes/businessunits.routes');
 const employeesRoutes = require('./routes/employees.routes');
+const cashboxesRoutes = require('./routes/cashboxes.routes');
 
 //USE ROUTES
 app.use( pathApi, businessunitsRoutes );
 app.use( pathApi, employeesRoutes );
+app.use( pathApi, cashboxesRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server UP corriendo en http://localhost:${process.env.PORT}`);
