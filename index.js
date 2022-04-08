5833bb3e842ed3708255c52bd72305301061ee72
May <myvd12@gmail.com>
2022-04-08 12:28:59 -0500

    charges CRUD and routes

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ app.use( cors() );
 //ROUTES
 const businessunitsRoutes = require('./routes/businessunits.routes');
 const employeesRoutes = require('./routes/employees.routes');
+const chargesRoutes = require('./routes/charges.routes');
 
 //USE ROUTES
 app.use( pathApi, businessunitsRoutes );
 app.use( pathApi, employeesRoutes );
+app.use( pathApi, chargesRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server UP corriendo en http://localhost:${process.env.PORT}`);
