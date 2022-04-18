258bb125c0a115f32ab8f7b558058e5e7778fa97
GeorginaPerez <xoielucker@gmail.com>
2022-04-18 08:46:22 -0500

    endpoints denominations

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ app.use( cors() );
 //ROUTES
 const businessunitsRoutes = require('./routes/businessunits.routes');
 const employeesRoutes = require('./routes/employees.routes');
+const denominationsRoutes = require('./routes/denominations.routes');
 
 //USE ROUTES
 app.use( pathApi, businessunitsRoutes );
 app.use( pathApi, employeesRoutes );
+app.use(pathApi, denominationsRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server UP corriendo en http://localhost:${process.env.PORT}`);
