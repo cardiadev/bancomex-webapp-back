6548434376147d61683ddcf623029c14eb587e47
AlexeiNay <alexeisanchez66@gmail.com>
2022-04-18 09:28:51 -0500

    Bugs Fixed

--- models/credit.js
+++ models/credit.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Credit.belongsTo(models.Cliente);
+      Credit.belongsTo(models.Client);
       Credit.belongsTo(models.Employee);
       Credit.hasMany(models.Guarantee);
     }
