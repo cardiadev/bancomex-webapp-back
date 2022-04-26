427527c89fb4f6b14d115cd62640085f9f7d35e2
DanielFlores1223 <dany-232000@hotmail.com>
2022-04-26 08:35:11 -0500

    clients create finished

--- controllers/clients.controller.js
+++ controllers/clients.controller.js
@@ -1,6 +1,8 @@
 const { generateQR } = require('../common/functions/generateqr');
-const { sendEmail } = require('../common/mailService/mail.service');
+const { sendEmail, sendEmailWithCard } = require('../common/mailService/mail.service');
 const Model = require('../models').Client
+const Account = require("../models").Account
+const Card = require('../models').Card
 const nameModel = 'Client';
 
 const findAll = async(req,res) => {
@@ -32,24 +34,61 @@ const findOne = async (req,res) => {
 
 const create = async (req, res) => {
     try{
-        console.log(req.user);
         if (req.user.role == 'Cajero') 
             return res.status(401).json({ msg: 'Denied Role Access' })
-            
-        req.body.EmployeeId = req.user.id;
-        req.body.active = true;
-        req.body.BranchId = 1;
+        
+        //verify If the client exists
+        const clientExist = await Model.findOne( { where: req.body.curp } );
 
-        const result = await Model.create({ ...req.body});
+        if (clientExist) 
+            return res.status(400).send({success: false, msg: `This ${req.body.client.curp} is already registered`});
+
+        req.body.EmployeeId = req.user.id;
+        req.body.client.active = true;
+        req.body.client.BranchId = 1;
+
+        const result = await Model.create({ ...req.body.client });
+        
+        //crate account
+        req.body.account.ClientId = result.id;
+        const date = new Date();
+        req.body.account.dateCreate = date.toISOString();
+        req.body.account.amount = 0;
+        req.body.account.state = true; 
+        const resultAc = await Account.create({ ...req.body.account });
+
+        //create card
+        const cardInfo = {
+            cardNumber: 0,
+            nip: Math.floor(Math.random() * (9999 - 1000)) + 1000,
+            dateExpiration: '',
+            state: true,
+            AccountId: resultAc.id
+        }
+
+        const newCardNumber = '4125874' + 
+                              (resultAc.type == 'Credito' ? '1' : '2') + 
+                              ( 1000 + resultAc.id ) + 
+                              ( Math.floor(Math.random() * (9999 - 1000)) + 1000 );
+
+        cardInfo.cardNumber = newCardNumber;
+
+        cardInfo.dateExpiration = new Date(new Date().setFullYear(new Date().getFullYear() + 4));
+
+        const cardCreated = await Card.create({ ...cardInfo });
+
         //create QR end send the QR for email.
         generateQR('ID:'+result.id).then(url => {
             sendEmail(result, url)
-          })
+        });
+
+        sendEmailWithCard(result, cardCreated);
+
         res
             .status(201)
-            .send({succes: true, result, msg:`${nameModel} was created succesfully`});
+            .send({succes: true, result: { client: result, account: resultAc, card: cardCreated }, msg:`${nameModel} was created succesfully`});
 
-    }catch(error){
+    } catch(error){
         res
             .status(400)
             .send({succes: false, msg:`${nameModel} wasn't created`, error})
